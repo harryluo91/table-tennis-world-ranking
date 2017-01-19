d742eeff647165c0ae783e9a4ac31ee3987e9f0d
Harry Luo <hluo@konradgroup.com>
2017-01-19 11:11:23 -0500

    more updates

--- app/match.service.ts
+++ app/match.service.ts
@@ -8,45 +8,46 @@ import { Match } from './match';
 export class MatchService {
     private headers = new Headers({'Content-Type': 'application/json'});
     private matchesUrl = 'api/matches';  // URL to web api
-    private matchesUrlSails = 'http://localhost:1337/api/matches';
-    private matchesUrlJSON = 'http://localhost:1338/api/matches';
+    // private matchesUrlSails = 'http://localhost:1337/api/matches';
+    // private matchesUrlJSON = 'http://localhost:1338/api/matches';
 
     constructor(private http: Http) { }
 
-    getPlayerMatches(id: number): Promise<Match[]> {
-        return this.http.get(this.matchesUrlJSON)
-                .toPromise()
-                .then((response) => {
-                    return this.playerMatchSelector(response.json().data, id) as Match[];
-                })
-                .catch(this.handleError);
-    }
-
-    updateMatchScores(match: Match): Promise<Match> {
-        var matchId = match.id;
-        var scores = {
-            playerOnePoints: match.playerOnePoints,
-            playerTwoPoints: match.playerTwoPoints
-        }
-        return this.http.put(`${this.matchesUrlJSON}/${matchId}`, JSON.stringify(match), {headers: this.headers})
-            .toPromise()
-            .then(() => match)
-            .catch(this.handleError);
-    }
-
-    private playerMatchSelector(matches: Match[], id: number): Array<Match> {
-        return matches.filter((match) => {
-            return match.playerOne.id === id || match.playerTwo.id === id;
-        })
-    }
-
-    private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
-    }
-
 }
 
+// getPlayerMatches(id: number): Promise<Match[]> {
+//     return this.http.get(this.matchesUrlJSON)
+//             .toPromise()
+//             .then((response) => {
+//                 return this.playerMatchSelector(response.json().data, id) as Match[];
+//             })
+//             .catch(this.handleError);
+// }
+
+// updateMatchScores(match: Match): Promise<Match> {
+//     var matchId = match.id;
+//     var scores = {
+//         playerOnePoints: match.playerOnePoints,
+//         playerTwoPoints: match.playerTwoPoints
+//     }
+//     return this.http.put(`${this.matchesUrlJSON}/${matchId}`, JSON.stringify(match), {headers: this.headers})
+//         .toPromise()
+//         .then(() => match)
+//         .catch(this.handleError);
+// }
+
+// private playerMatchSelector(matches: Match[], id: number): Array<Match> {
+//     return matches.filter((match) => {
+//         return match.playerOne.id === id || match.playerTwo.id === id;
+//     })
+// }
+
+// private handleError(error: any): Promise<any> {
+//     console.error('An error occurred', error); // for demo purposes only
+//     return Promise.reject(error.message || error);
+// }
+
+// ***************************************************************************
 // getPlayerMatches(playerId: number): Promise<Match[]> {
 //     return this.http.get(`${this.baseUrl}/matches/${playerId}`)
 //             .toPromise()
--- app/player.service.ts
+++ app/player.service.ts
@@ -8,56 +8,56 @@ import { Player } from './player';
 export class PlayerService {
     private headers = new Headers({'Content-Type': 'application/json'});
     private playersUrl = 'api/players';  // URL to web api
-    private playersUrlSails = 'http://localhost:1337/api/players';
-    private playersUrlJSON = 'http://localhost:1338/api/players';
+    // private playersUrlSails = 'http://localhost:1337/api/players';
+    // private playersUrlJSON = 'http://localhost:1338/api/players';
 
     constructor(private http: Http) { }
 
-    getPlayers(): Promise<Player[]> {
-        return this.http.get(this.playersUrlJSON)
-                .toPromise()
-                .then(response => response.json().data as Player[])
-                .catch(this.handleError);
-    }
-
-    getPlayer(id: number): Promise<Player> {
-        const url = `${this.playersUrlJSON}/${id}`;
-        return this.http.get(url)
-            .toPromise()
-            .then(response => {
-                return response.json().data as Player
-            })
-            .catch(this.handleError);
-    }
-
-    updatePlayer(player: Player): Promise<Player> {
-        const url = `${this.playersUrlJSON}/${player.id}`;
-        return this.http
-            .put(url, JSON.stringify(player), {headers: this.headers})
-            .toPromise()
-            .then(() => player)
-            .catch(this.handleError);
-    }
-
-    create(firstName: string, lastName: string): Promise<Player> {
-        return this.http
-            .post(this.playersUrlJSON, JSON.stringify({firstName: firstName, lastName: lastName}), {headers: this.headers})
-            .toPromise()
-            .then(res => res.json().data)
-            .catch(this.handleError);
-    }
-
-    delete(id: number): Promise<void> {
-        const url = `${this.playersUrlJSON}/${id}`;
-        return this.http.delete(url, {headers: this.headers})
-            .toPromise()
-            .then(() => null)
-            .catch(this.handleError);
-    }
-
-    private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
-    }
-
 }
+
+// getPlayers(): Promise<Player[]> {
+//     return this.http.get(this.playersUrlJSON)
+//             .toPromise()
+//             .then(response => response.json().data as Player[])
+//             .catch(this.handleError);
+// }
+
+// getPlayer(id: number): Promise<Player> {
+//     const url = `${this.playersUrlJSON}/${id}`;
+//     return this.http.get(url)
+//         .toPromise()
+//         .then(response => {
+//             return response.json().data as Player
+//         })
+//         .catch(this.handleError);
+// }
+
+// updatePlayer(player: Player): Promise<Player> {
+//     const url = `${this.playersUrlJSON}/${player.id}`;
+//     return this.http
+//         .put(url, JSON.stringify(player), {headers: this.headers})
+//         .toPromise()
+//         .then(() => player)
+//         .catch(this.handleError);
+// }
+
+// create(firstName: string, lastName: string): Promise<Player> {
+//     return this.http
+//         .post(this.playersUrlJSON, JSON.stringify({firstName: firstName, lastName: lastName}), {headers: this.headers})
+//         .toPromise()
+//         .then(res => res.json().data)
+//         .catch(this.handleError);
+// }
+
+// delete(id: number): Promise<void> {
+//     const url = `${this.playersUrlJSON}/${id}`;
+//     return this.http.delete(url, {headers: this.headers})
+//         .toPromise()
+//         .then(() => null)
+//         .catch(this.handleError);
+// }
+
+// private handleError(error: any): Promise<any> {
+//     console.error('An error occurred', error); // for demo purposes only
+//     return Promise.reject(error.message || error);
+// }
